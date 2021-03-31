74148e438658b5677e241cc4413138825d2a83f3
mayralgr <ml17ft@gmail.com>
2021-03-30 20:05:07 -0600

    Add react router module

--- src/App.js
+++ src/App.js
@@ -3,18 +3,22 @@ import AboutPage from "./components/AboutPage";
 import Header from "./components/common/Header";
 import CoursesPage from "./components/CoursesPage";
 import HomePage from "./components/HomePage";
+import { Route, Switch, Redirect } from "react-router-dom";
+import NotFoundPage from "./components/NotFoundPage";
+import ManageCoursePage from "./components/ManageCoursePage";
 
 function App() {
-  function getPage() {
-    const route = window.location.pathname;
-    if (route === "/courses") return <CoursesPage />;
-    if (route === "/about") return <AboutPage />;
-    return <HomePage />;
-  }
   return (
     <div className="container-fluid">
       <Header />
-      {getPage()}
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/courses" component={CoursesPage} />
+        <Route path="/about" component={AboutPage} />
+        <Route path="/course/:slug" component={ManageCoursePage} />
+        <Redirect from="/about-page" to="/about" />
+        <Route component={NotFoundPage} />
+      </Switch>
     </div>
   );
 }
